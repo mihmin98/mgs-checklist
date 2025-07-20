00f2dfd133a8ca499fe847bd00232a71814c76c7
mihmin98 <mihmin98@yahoo.com>
2025-07-20 12:18:54 +0300

    added bootstrap

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// Import our custom CSS
+import './scss/styles.scss'
+
+// Import all of Bootstrap's JS
+// import * as bootstrap from 'bootstrap'
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // root: path.resolve(__dirname, 'src'),
+  resolve: {
+    alias: {
+      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+    }
+  },
   plugins: [react()],
 })
