0ef286b734b707b03432a1813bc96a3d1de2f8f7
mihmin98 <mihmin98@yahoo.com>
2025-07-23 21:02:53 +0300

    small fixes

--- src/components/ItemList/ItemList.tsx
+++ src/components/ItemList/ItemList.tsx
@@ -2,7 +2,7 @@ import { appData } from "../../data/data";
 import ItemCard from "../ItemCard/ItemCard";
 
 export default function ItemList() {
-  const content = appData.items
+  const content = [...appData.items]
     .sort((a, b) => a.name.localeCompare(b.name))
     .map(item => <ItemCard key={item.name} item={item} checkable={false} />);
 
